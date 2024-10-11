94fe9e4a5305902c97fc254f3c12b4010bfacab8
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2024-10-11 10:24:26 -0300

    Orden Noticias

--- resources/views/content/news/noticias.blade.php
+++ resources/views/content/news/noticias.blade.php
@@ -45,10 +45,6 @@
                                     class="text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100">
                                     Más Antiguo al más Nuevo
                                 </a>
-                                <a href="{{ url()->current() }}?orden=3"
-                                    class="text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100">
-                                    Mayor número de Interacciones
-                                </a>
                             </nav>
                         </div>
                     </div>
