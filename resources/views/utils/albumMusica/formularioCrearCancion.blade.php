6caae48a292ee6f9b19e87a5e937e4f72db13baa
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2024-10-19 18:55:53 -0300

    Nada, solo Visual

--- resources/views/utils/albumMusica/formularioCrearCancion.blade.php
+++ resources/views/utils/albumMusica/formularioCrearCancion.blade.php
@@ -1,3 +1,61 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-</div>
+<x-AppLayout>
+    <div class="flex flex-col mx-3 mt-6 lg:flex-row">
+        <div class="w-full lg:w-1/3 m-1">
+            <form class="w-full bg-white shadow-md p-6">
+                <h1 class='flex font-extrabold text-2xl mb-2 
+                justify-center'>NOMBRE DEL ALBUM: </h1>
+                <div class="flex flex-wrap -mx-3 mb-6">
+
+                    <div class="w-full md:w-full px-3 mb-6">
+                        <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="category_name">Nombre de Canción</label>
+                        <input
+                            class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]"
+                            type="text" name="name" placeholder="Nombre de Canción" required />
+                    </div>
+
+                    <div class="w-full px-3 mb-6">
+                        <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="category_name">Letra de Canción en Español</label>
+                        <textarea textarea rows="4"
+                            class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]"
+                            type="text" name="description" required> </textarea>
+                    </div>
+
+                    <div class="w-full px-3 mb-6">
+                        <label class="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2"
+                            htmlFor="category_name">Letra de Canción en Ingles</label>
+                        <textarea textarea rows="4"
+                            class="appearance-none block w-full bg-white text-gray-900 font-medium border border-gray-400 rounded-lg py-3 px-3 leading-tight focus:outline-none focus:border-[#98c01d]"
+                            type="text" name="description" required> </textarea>
+                    </div>
+                    <div class="w-full md:w-full px-3 mb-6">
+                        <button
+                            class="appearance-none block w-full bg-green-700 text-gray-100 font-bold border border-gray-200 rounded-lg py-3 px-3 leading-tight hover:bg-green-600 focus:outline-none focus:bg-white focus:border-gray-500">Agregar
+                            Cancion al Album</button>
+                    </div>
+
+                    <div class="w-full px-3 mb-8">
+                        {{-- Aca deberia ser Insertar Mp3 --}}
+                        <label
+                            class="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center justify-center rounded-xl border-2 border-dashed border-green-400 bg-white p-6 text-center"
+                            htmlFor="dropzone-file">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-green-800" fill="none"
+                                viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                            </svg>
+
+                            <h2 class="mt-4 text-xl font-medium text-gray-700 tracking-wide">Archivo de la Canción</h2>
+
+                            <p class="mt-2 text-gray-500 tracking-wide">Inserte la canción. </p>
+
+                            <input id="dropzone-file" type="file" class="hidden" name="category_image"
+                                accept="image/png, image/jpeg, image/webp" />
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</x-AppLayout>
