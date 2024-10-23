e5bcff5436f23d2c5bd511e54f9d0f0af82d6397
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2024-10-23 18:50:26 -0300

    Avance 2 Descarga

--- resources/views/content/descargas.blade.php
+++ resources/views/content/descargas.blade.php
@@ -18,32 +18,12 @@
                         <tr>
                             <td class="py-4 px-4 border-b border-gray-200">
                                 {{-- Muestra imagen/video o cancion --}}
-                                @if (!empty($item['imagenes']))
-                                    {{-- Muestra la primera imagen --}}
-                                    <img src="{{ asset($item['imagenes'][0]['rutaImagen']) }}" alt="Imagen"
-                                        class="w-16 h-16 object-cover">
-                                @elseif (!empty($item['videos']))
-                                    {{-- Muestra el primer video --}}
-                                    <video width="100" height="100" controls>
-                                        <source src="{{ asset($item['videos'][0]['rutaVideo']) }}" type="video/mp4">
-                                        Tu navegador no soporta el video.
-                                    </video>
-                                @elseif (!empty($item['canciones']))
-                                    {{-- Muestra el primer audio --}}
-                                    <audio controls>
-                                        <source src="{{ asset($item['canciones'][0]['rutaCancion']) }}"
-                                            type="audio/mpeg">
-                                        Tu navegador no soporta el audio.
-                                    </audio>
-                                @else
-                                    {{-- Muestra un mensaje si no hay contenido --}}
-                                    <span>No hay contenido</span>
-                                @endif
                             </td>
                             <td class="py-4 px-4 border-b font-semibold border-gray-200">{{ $item['tituloAlbum'] }}</td>
                             <td class="py-4 px-4 border-b border-gray-200 truncate">{{ $item['fechaAlbum'] }}</td>
                             <td class="py-4 px-4 border-b border-gray-200">Imagen/Video/Cancion
                                 {{-- Dice que tipo de dato es --}}
+
                             </td>
                             <td class="py-4 px-4 border-b border-gray-200">
                                 <span class="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Si / No</span>
