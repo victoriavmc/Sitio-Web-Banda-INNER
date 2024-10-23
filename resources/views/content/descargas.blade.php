c5bd85edbee2ecc3f964d9f093db364486d400d7
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2024-10-23 18:36:12 -0300

    Avance 2 Descarga

--- resources/views/content/descargas.blade.php
+++ resources/views/content/descargas.blade.php
@@ -3,7 +3,9 @@
         <table class="w-full table-fixed">
             <thead>
                 <tr class="bg-gray-100">
-                    <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Archivo</th>
+                    <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Muestra</th>
+                    <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Titulo Album</th>
+                    <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Fecha Album</th>
                     <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Tipo</th>
                     <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Descargar</th>
                     <th></th>
@@ -11,6 +13,24 @@
             </thead>
             <tbody class="bg-white">
                 @dd($albumInfo)
+                @foreach ($albumInfo as $item)
+                    <tr>
+                        <td class="py-4 px-6 border-b border-gray-200"></td>
+                        <td class="py-4 px-6 border-b border-gray-200">{{ $item['tituloAlbum'] }}</td>
+                        <td class="py-4 px-6 border-b border-gray-200 truncate">{{ $item['fechaAlbum'] }}</td>
+
+                        <td class="py-4 px-6 border-b border-gray-200">
+                            <span class="bg-green-500 text-white py-1 px-2 rounded-full text-xs">Active</span>
+                        </td>
+                        {{-- <td class="py-4 px-6 border-b border-gray-200">
+                        <span class="bg-red-500 text-white py-1 px-2 rounded-full text-xs">Inactive</span>
+                    </td> --}}
+                        <td class="p-3 px-5 flex justify-end">
+                            <button type="button"
+                                class="mr-3 text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Modificar</button>
+                        </td>
+                    </tr>
+                @endforeach
                 <tr>
                     <td class="py-4 px-6 border-b border-gray-200">John Doe</td>
                     <td class="py-4 px-6 border-b border-gray-200 truncate">johndoe@gmail.com</td>
