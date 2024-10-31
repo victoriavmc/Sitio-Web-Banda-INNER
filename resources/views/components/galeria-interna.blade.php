38f6dc53f5683285ff62f0d28d4f71f45c5d0ff2
SantiAranda <santiago.aranda.81@gmail.com>
2024-10-31 01:50:45 -0300

    Arreglo Menor Galeria Interna

--- resources/views/components/galeria-interna.blade.php
+++ resources/views/components/galeria-interna.blade.php
@@ -64,28 +64,30 @@
             @foreach ($medios as $idMuestra => $media)
                 <div class="relative">
                     <div class="absolute w-full flex justify-end">
-                        @if (Auth::user()->rol->idrol === 1 || Auth::user()->rol->idrol === 2)
-                            {{-- Condición para no mostrar el botón de eliminar para la primera imagen --}}
-                            @if ($idMuestra !== array_key_first($medios))
-                                {{-- Botón de borrar contenido --}}
-                                <form action="{{ route('eliminar.objeto') }}" method="POST" class="z-30 pt-2 pr-2">
-                                    @csrf
-                                    @method('POST')
-                                    <input type="hidden" name="tipo" value="{{ $tipo }}">
-                                    <input type="hidden" name="idAlbumEspecifico" value="{{ $idMuestra }}">
-                                    <button type="submit"
-                                        class="bg-red-500 hover:bg-red-400 text-white text-xs p-1 font-bold border-b-4 border-red-700 hover:border-red-500 rounded w-max">
-                                        <svg class="w-5 h-5 text-white" aria-hidden="true"
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            fill="none" viewBox="0 0 24 24">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                                stroke-width="2"
-                                                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                        </svg>
-                                    </button>
-                                </form>
+                        @auth
+                            @if (Auth::user()->rol->idrol === 1 || Auth::user()->rol->idrol === 2)
+                                {{-- Condición para no mostrar el botón de eliminar para la primera imagen --}}
+                                @if ($idMuestra !== array_key_first($medios))
+                                    {{-- Botón de borrar contenido --}}
+                                    <form action="{{ route('eliminar.objeto') }}" method="POST" class="z-30 pt-2 pr-2">
+                                        @csrf
+                                        @method('POST')
+                                        <input type="hidden" name="tipo" value="{{ $tipo }}">
+                                        <input type="hidden" name="idAlbumEspecifico" value="{{ $idMuestra }}">
+                                        <button type="submit"
+                                            class="bg-red-500 hover:bg-red-400 text-white text-xs p-1 font-bold border-b-4 border-red-700 hover:border-red-500 rounded w-max">
+                                            <svg class="w-5 h-5 text-white" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                fill="none" viewBox="0 0 24 24">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                @endif
                             @endif
-                        @endif
+                        @endauth
                     </div>
                     <div class="relative">
                         @if ($tipo == 2)
